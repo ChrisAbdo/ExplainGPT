42240dd43da234bbd11038b815f32fed0fcd9e2d
chris abdo <chris.j.abdo@gmail.com>
2023-02-05 00:44:01 -0500

    ADDED: updates to env

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -25,7 +25,7 @@ import {
     const res = await fetch("https://api.openai.com/v1/completions", {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY ?? ""}`,
       },
       method: "POST",
       body: JSON.stringify(payload),
